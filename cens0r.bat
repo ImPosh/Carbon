934a783db9230f1fb9c6bb5ac31c534e4be680ea
posh <101807149+ImPosh@users.noreply.github.com>
2023-03-29 11:48:00 -0400

    Fixed Cmd Popping Up
    The CMD would pop up when running the bat file so I fixed it

--- cens0r.bat
+++ cens0r.bat
@@ -1,7 +1,7 @@
 @echo off
 color 0a
 echo Welcome to cens0r!
-echo Made by posh#5696
+echo Made by posh#5696 and stonks#7903
 Pause. >nul | echo.Do you want to end censorship on this device. If so click any key to countinue with the setup
 Pause. >nul | echo.Do you understand that this program is made for fcps computers using lightspeed. click any key to continue
 echo " ___________           .___ _________                                         .__    .__     " 
@@ -21,6 +21,5 @@ echo ...
 echo Download Completed
 Pause. >nul | echo.Click any key to close chrome you need do this so this will work properly (this wont damage your device)
 TASKKILL /IM chrome.exe /F
-start
 Pause. >nul | echo.Chrome has been closed click any key to close this program and open a new tab in chrome (enjoy!)
-start chrome "www.google.com/search?aqs=chrome..69i57j33i160.13049j0j7&ie=UTF-8&oq=everything+is+unblocked+%28enjoy%21%29&q=everything+is+unblocked+%28enjoy%21%29&rlz=1C1GCEA_enUS1028US1028&sourceid=chrome&surl=1&safe=active&ssui=on"
+start chrome "www.google.com"
